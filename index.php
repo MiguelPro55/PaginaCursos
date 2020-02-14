8a25160aa2cc5673f606c2f63894f030350576df
AlexGtz26 <44044026+AlexGtz26@users.noreply.github.com>
2020-02-13 19:46:27 -0600

    implementacion de css

--- index.php
+++ index.php
@@ -5,48 +5,7 @@ echo $_SERVER['HTTP_USER_AGENT'];
 <!DOCTYPE html>
 <html>
 <head>
-    <style>
-        .lineaslargas {
-            justify-content: center;
-            background-color: black;
-            color: white;
-            margin: 10px;
-            padding: 20px;
-            }
-        .lineaslargas:hover{
-            background-color:white ;
-            color:black;
-            margin: 5px;
-            padding: 20px;
-        }
-        ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-            background-color: #333;
-        }
-
-        li {
-            float: left;
-        }
-
-        li a {
-            display: block;
-            color: white;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-        }
-
-        /* Change the link color to #111 (black) on hover */
-        li a:hover {
-            background-color:white;
-            color:black;
-        }
-
-
-    </style>
+<link rel="stylesheet" href="complemento.css">
 </head>
 <body>
 
