89c9c62c48ac0ff29542df25b0806e719d54e0f8
CarlosOlvera <carloscmasmas@gmail.com>
2020-02-18 16:36:46 -0600

    mensaje

---
+++ about.php
@@ -0,0 +1,226 @@
+<?php
+echo "<link rel=stylesheet href=complemento.css>";
+echo "somos un equipo de desarrolladores";
+echo "<p>ADMINISTRACIÓN
+DE REDES
+1.
+FUNCIONES DE LA
+ADMINISTRACIÓN DE
+REDES
+1.1 CONFIGURACIÓN
+Un administrador de red sirve a los usuarios: crea espacios de comunicación,
+atiende sugerencias; mantiene las herramientas y el espacio requerido por
+cada usuario, a tiempo y de buena forma (piense si usted fuera usuario como
+le gustaría que fuera el administrador); mantiene en buen estado el hardware
+y el software de los computadores y la(s) red(es) a su cargo; mantiene
+documentación que describe la red, el hardware y el software que administra;
+respeta la privacidad de los usuarios y promueve el buen uso de los recursos.
+</p>
+<p>1.1 CONFIGURACIÓN
+A cambio de tantas responsabilidades la recompensa es el buen
+funcionamiento de la red como un medio que vincula personas y de los
+computadores y programas como herramientas para agilizar algunas labores
+que dan tiempo y dar tiempo para realizar otras.
+El administrador de red debe conocer las reglas de la cuenta principal de las
+máquinas que administra. Desde esa cuenta puede configurar servicios y
+establecer políticas que afectarán a todos los usuarios.
+Algunas de las labores que sólo pueden hacerse desde esta cuenta son:
+
+1.1 CONFIGURACIÓN
+▰ Apagar el equipo
+▰ Configurar los programas que se inician junto con el sistema.
+▰ Administrar cuentas de usuarios.
+▰ Administrar los programas y la documentación instalada.
+▰ Configurar los programas y los dispositivos.
+▰ Configurar la zona geográfica, fecha y hora.
+▰ Administrar espacio en discos y mantener copias de respaldo.
+▰ Configurar servicios que funcionarán en red.
+▰ Solucionar problemas con dispositivos o programas.
+</p> 
+<p>
+Las actividades que se
+desempeñan en la
+administración de
+configuración son:
+
+La planeación y diseño de la red
+La meta de esta actividad es satisfacer los requerimientos inmediatos y
+futuros de la red. Contempla varias etapas, algunas son:
+Reunir las necesidades de la red.
+Diseñar la topología de la red.
+Determinar y seleccionar la infraestructura de red basada en los
+requerimientos técnicos y en la topología propuesta.
+En caso de redes grandes, diseñar la distribución de tráfico mediante algún
+mecanismo de ruteo.
+Si el servicio satisface las necesidades, se debe planear a su implementación.
+</p>
+<p>
+La administración del software
+Es la actividad responsable del manejo adecuado de recursos de
+hardware y software dentro de la red, comprende la instalación,
+desinstalación y actualización de una aplicación, sistema
+operativo o funcionalidad en los dispositivos de la red. Además,
+de mantener un control sobre los programas que son creados
+para obtener información específica en los dispositivos.
+
+El aprovisionamiento
+Esta tarea tiene la función de asegurar la redundancia de los elementos de
+software y hardware mas importantes de la red. Se encarga de:
+▰ Aprovisionar e instalar equipos.
+▰ Poseer información sobre el diseño de la red, su configuración y estado.
+▰ Poseer una base de datos que contenga diversidad de información
+(elementos de la red, historia de modificaciones y problemas, nombres de
+dominio)
+▰ Realizar mantenimiento de directorios.
+▰ Coordinar el esquema de nombres de nodos y aplicaciones.
+
+El aprovisionamiento
+▰ Mantener un control operacional de la red.
+▰ Proporcionar configuración de dispositivos.
+▰ Cargar y configurar versiones de configuraciones.
+▰ Realizar actualizaciones de Software/ Hardware.
+
+El aprovisionamiento
+La gestión de configuración debe estar al tanto de:
+▰ ¿Qué está instalado?
+▰ ¿Dónde está instalado?
+▰ ¿Cómo está conectado?
+▰ ¿Quién responde por cada cosa?
+▰ ¿Cómo comunicarse con los responsables?
+▰ Estado operacional de los elementos de la red\"
+1.2 FALLAS
+Se encarga de todos los procedimientos para el manejo de los
+sistemas de alarmas, equipos de prueba, además proporciona
+respuestas inteligentes a las quejas de los clientes y realiza
+manejo de incidencias.
+Para saber si existe un problema en la red se cuentan con varios
+mecanismos como se mencionan a continuación:
+
+▰ Herramientas de
+monitoreo
+▰ Utilidades Comunes
+▰ Ping
+▰ Traceroute
+▰ Sniffer: Ethereal
+1.2 FALLAS
+▰ Snmp
+▰ Sistemas de Monitoreo
+▰ HP Openview
+▰ Nagios
+▰ Big Brother
+▰ Reportes de estado
+
+“ La administración de fallas
+maneja las condiciones de
+error en todos los
+componentes de la red, en
+las siguientes fases:
+
+MONITOREO DE ALARMAS
+Se realiza la notificación de la existencia de una falla y del lugar donde se ha
+generado, esto se puede realizar con el auxilio de las herramientas basadas en
+el protocolo SNMP. Las alarmas pueden ser caracterizadas por su tipo y su
+severidad.
+Tipos de alarmas.- Alarmas en las comunicaciones, de procesos, de equipos,
+ambientales y en el servicio.
+Severidad de las alarmas.- Crítica, mayor, menor e indefinida.
+
+LOCALIZACIÓN DE FALLAS
+Determinar el origen de la falla.
+
+PRUEBAS DE DIAGNÓSTICO
+Diseñar y realizar pruebas que apoyen la localización de una falla
+(Pruebas de conectividad física, pruebas de conectividad lógica,
+pruebas de medición).
+
+CORRECCIÓN DE FALLAS
+Es la etapa donde se recuperan las fallas, las cuales pueden
+depender de la tecnología de red.
+
+ADMINISTRACIÓN DE REPORTES
+Registrar y dar seguimiento a todos los reportes generados por
+los usuarios o por el mismo administrador de la red.
+
+CICLO DE VIDA DE LA
+ADMINISTRACIÓN DE REPORTES
+▰ Creación de Reportes.- Un reporte es creado después de haber recibido
+una notificación sobre la existencia de un problema en la red.
+▰ Seguimiento a Reporte.- La administración de reportes debe permitir al
+administrador dar seguimiento de cada acción tomada para solucionar el
+problema, y conocer el estado histórico y actual del reporte.
+▰ Manejo de Reportes.- El administrador debe ser capaz de tomar ciertas
+acciones cuando un reporte está en curso.
+▰ Finalización de Reportes.- Una vez que el problema reportado ha sido
+solucionado, el administrador del sistema de reportes, debe dar por
+cerrado el reporte.
+
+1.3 CONTABILIDAD
+Se encarga del conteo del tiempo de conexión y terminación,
+número de mensajes transmitidos y recibidos, nombre del punto
+de acceso al servicio, razón por la que terminó la conexión y el
+control de Inventarios.
+Este servicio provee datos concernientes al cargo por uso de la
+red. Entre los datos proporcionados están los siguientes:
+▰ Tiempo de conexión y terminación.
+▰ Número de mensajes transmitidos y recibidos.
+▰ Nombre del punto de acceso al servicio.
+▰ Razón por la que terminó la conexión.
+1.3 CONTABILIDAD.
+1.3 CONTABILIDAD.
+Se encarga también de las mediciones, control de costos y
+facturación de los clientes.
+La estadística de red es de gran importancia, ya que esta
+permite el manejo de la contabilidad, la resolución de problemas,
+la realización de pronósticos a largo plazo, la planificación de la
+capacidad entre otras.
+1.4 DESEMPEÑO
+Se encarga de las actividades que mantienen la visión a corto
+plazo de la calidad de los servicios (QoS).
+Tiene como objetivo asegurar el funcionamiento óptimo de la
+red, lo que incluye: El número de paquetes que se transmiten por
+segundo, tiempos pequeños de respuesta y disponibilidad de la
+red.
+1.4 DESEMPEÑO.
+La administración del rendimiento se divide en 2 etapas:
+monitoreo y análisis.
+▰ Monitoreo.-consiste en observar y recolectar la información referente al
+comportamiento de la red.
+▰ Análisis.-Una vez recolectada la información mediante la actividad de
+monitoreo, es necesario interpretarla para determinar el comportamiento
+de la red y tomar decisiones adecuadas que ayuden a mejorar su
+desempeño.
+1.5 SEGURIDAD
+Se encarga de controlar el acceso de los clientes a la red de datos
+y a los recursos.
+La estructura administrativa de la red debe tener mecanismos de
+seguridad buenos para lo siguiente: Identificación y
+autentificación del usuario, una clave de acceso y un password,
+autorización de acceso a los recursos, es decir, solo personal
+autorizado.
+1.5 SEGURIDAD
+Tiene como objetivo controlar el acceso a los recursos de la red
+de acuerdo a lo establecido localmente de modo que la red no
+pueda ser saboteada y que no pueda accederse a información
+importante sin la debida autorización. Estos sistemas trabajan
+subdividiendo los recursos de la red en áreas autorizadas y áreas
+no autorizadas.
+Generalmente involucra los siguientes pasos:
+▰ Identifica los recursos sensibles de la red.
+▰ Determina correspondencia entre recursos sensibles de
+la red y series de usuarios.
+▰ Monitorea los puntos de acceso a recursos sensibles de
+la red.
+▰ Registra los accesos no autorizados a recursos sensibles
+de la red.</p>";
+$nosotros = array("Carlos Daniel Olvera Magno","Jose Miguel Procopio Esteban","Ivan Alejandro Gutierrez Garcia","Christian Cuellar","Adolfo Taquillo");
+
+echo "<title>Grupo de trabajo</title>";
+echo "<h1>DREAM TEAM</h1>";
+
+for ($i=0;$i<=4;$i++){
+    echo "<div class=nombresprogramadores>";
+    echo "<br><h2>Programador: $nosotros[$i]</h2>";
+    echo "</div>";
+}
+
+?>
--- index.php
+++ index.php
@@ -1,51 +1,33 @@
 <?php
-
 echo $_SERVER['HTTP_USER_AGENT'];
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <link rel="stylesheet" href="complemento.css">
+    <ul>
+        <li><a href="default.asp">Inicio</a></li>
+        <li><a href="news.asp">Login</a></li>
+        <li><a href="contact.asp">Resgitrate</a></li>
+        <li><a href="about.php">Nosotros</a></li>
+        <li><a href="#" ></a><li>
+    </ul>
 </head>
+
 <body>
 
-
-<ul>
-    <li><a href="default.asp">Home</a></li>
-    <li><a href="news.asp">News</a></li>
-    <li><a href="contact.asp">Contact</a></li>
-    <li><a href="about.asp">About</a></li>
-</ul>
-
 <h2 style="font-variant-ligatures: normal">Pagina de cursos</h2>
-
 <div class="lineaslargas">
     <h2>HTML</h2>
     <p>CURSO INICAL DE HTML.</p>
 </div>
-
 <div class="lineaslargas">
     <h2>JAVA</h2>
     <p>CURSO INICIAL DE JAVA.</p>
 </div>
-
 <div class="lineaslargas">
     <h2>C++</h2>
     <p>CURSO INICAL DE C++</p>
 </div>
-
-
-
 </body>
-
-
-
-
-
-
-
-
-
-
 </html>
-
